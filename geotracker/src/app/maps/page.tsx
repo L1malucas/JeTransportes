f58f974680d929b137b09a1411e43deb341dfd44
Lucas Lima <lucas.lima@suporterenova.onmicrosoft.com>
2025-01-30 23:48:50 -0300

    correção de deploy

--- geotracker/src/app/maps/page.tsx
+++ geotracker/src/app/maps/page.tsx
@@ -1,31 +1,36 @@
 // ./src/app/maps/page.tsx
+
 "use client";
 
-import React from "react";
+import React, { Suspense } from "react";
 import { useSearchParams } from "next/navigation";
 
-export default function MapsPage() {
+const MapsPage = () => {
   const searchParams = useSearchParams();
-  const location = searchParams.get("location") || "Estrada do coco"; // Valor padrão se nenhum parâmetro for fornecido
+  const location = searchParams.get("location") || "Estrada do coco"; // Default location if none is provided
 
-  // Gerar a URL do Google Maps para incorporação
+  // Function to generate the Google Maps embed URL
   const getGoogleMapsEmbedUrl = (loc: string) => {
     const encodedLocation = encodeURIComponent(loc);
     return `https://www.google.com/maps?q=${encodedLocation}&output=embed`;
   };
 
   return (
-    <div className="p-4 flex flex-col items-center ">
-      <div className="w-[90%] h-[90vh]">
-        <iframe
-          src={getGoogleMapsEmbedUrl(location)}
-          style={{ border: 0 }}
-          className="w-full h-full"
-          loading="lazy"
-          allowFullScreen
-          title="Mapa do Ônibus"
-        ></iframe>
+    <Suspense fallback={<div>Loading...</div>}>
+      <div className="p-4 flex flex-col items-center">
+        <div className="w-[90%] h-[90vh]">
+          <iframe
+            src={getGoogleMapsEmbedUrl(location)}
+            style={{ border: 0 }}
+            className="w-full h-full"
+            loading="lazy"
+            allowFullScreen
+            title="Mapa do Ônibus"
+          ></iframe>
+        </div>
       </div>
-    </div>
+    </Suspense>
   );
-}
+};
+
+export default MapsPage;
