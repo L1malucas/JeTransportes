e1eadd9ccefadb7b1276be45b0e8980133eecbf9
Lucas Lima <lucas.lima@suporterenova.onmicrosoft.com>
2025-01-30 23:54:06 -0300

    nova tentativa de corrigir o deploy

--- geotracker/src/app/maps/page.tsx
+++ geotracker/src/app/maps/page.tsx
@@ -2,34 +2,40 @@
 
 "use client";
 
-import React, { Suspense } from "react";
-import { useSearchParams } from "next/navigation";
+import React, { useEffect, useState } from "react";
 
 const MapsPage = () => {
-  const searchParams = useSearchParams();
-  const location = searchParams.get("location") || "Estrada do coco"; // Default location if none is provided
+  const [location, setLocation] = useState<string>("Estrada do coco");
 
-  // Function to generate the Google Maps embed URL
+  useEffect(() => {
+    // Pega o valor do parâmetro "location" da URL (se existir)
+    const queryParams = new URLSearchParams(window.location.search);
+    const locationFromUrl = queryParams.get("location");
+
+    if (locationFromUrl) {
+      setLocation(locationFromUrl); // Atualiza o estado com o valor da URL
+    }
+  }, []); // O array vazio faz a execução do useEffect apenas na montagem do componente
+
+  // Função para gerar a URL de incorporação do Google Maps
   const getGoogleMapsEmbedUrl = (loc: string) => {
     const encodedLocation = encodeURIComponent(loc);
     return `https://www.google.com/maps?q=${encodedLocation}&output=embed`;
   };
 
   return (
-    <Suspense fallback={<div>Loading...</div>}>
-      <div className="p-4 flex flex-col items-center">
-        <div className="w-[90%] h-[90vh]">
-          <iframe
-            src={getGoogleMapsEmbedUrl(location)}
-            style={{ border: 0 }}
-            className="w-full h-full"
-            loading="lazy"
-            allowFullScreen
-            title="Mapa do Ônibus"
-          ></iframe>
-        </div>
+    <div className="p-4 flex flex-col items-center">
+      <div className="w-[90%] h-[90vh]">
+        <iframe
+          src={getGoogleMapsEmbedUrl(location)}
+          style={{ border: 0 }}
+          className="w-full h-full"
+          loading="lazy"
+          allowFullScreen
+          title="Mapa do Ônibus"
+        ></iframe>
       </div>
-    </Suspense>
+    </div>
   );
 };
 
